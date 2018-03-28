fc2065a7fe8716ac416a479cb62cd7461b879f75
Eric Beyer <eric.m.beyer@gmail.com>
2018-03-28 10:45:58 -0700

    Update page with "create room" and "join room"
    * Join room simply increments the player counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Layout, Input, Button, List, Icon } from "antd";
+import firebase from "@firebase/app";
 
 // We import our firestore module
 import firestore from "./firestore";
@@ -12,93 +13,115 @@ class App extends Component {
   constructor(props) {
     super(props);
     // Set the default state of our application
-    this.state = { addingTodo: false, pendingTodo: "", todos: [] };
+    this.state = { creatingRoom: false, joiningRoom: false, rooms: [] };
     // We want event handlers to share this context
-    this.addTodo = this.addTodo.bind(this);
-    this.completeTodo = this.completeTodo.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+    this.joinRoom = this.joinRoom.bind(this);
     // We listen for live changes to our todos collection in Firebase
-    firestore.collection("todos").onSnapshot(snapshot => {
-      let todos = [];
+    firestore.collection("rooms").onSnapshot(snapshot => {
+      let rooms = [];
       snapshot.forEach(doc => {
-        const todo = doc.data();
-        todo.id = doc.id;
-        if (!todo.completed) todos.push(todo);
+        const room = doc.data();
+        room.id = doc.id;
+        if (!room.private) rooms.push(room);
       });
       // Sort our todos based on time added
-      todos.sort(function(a, b) {
+      rooms.sort(function(a, b) {
         return (
           new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
         );
       });
       // Anytime the state of our database changes, we update state
-      this.setState({ todos });
+      this.setState({ rooms });
     });
   }
+  
+  getHash() {
+    const length = 4;
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  async completeTodo(id) {
-    // Mark the todo as completed
-    await firestore
-      .collection("todos")
-      .doc(id)
-      .set({
-        completed: true
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+
+  async joinRoom(id) {
+    console.log('join room', id);
+    if (this.state.joiningRoom) return;
+    
+    this.setState({ joiningRoom: true });
+    let roomRef = await firestore
+      .collection("rooms")
+      .doc(id);
+      
+    let room = await roomRef.get();
+    console.log('room ', room);
+    let data = room.data();
+    console.log('data ', data);
+    let playerCount = data.playersInLobby || 1;
+    await roomRef
+      .update({
+        playersInLobby: playerCount+1
       });
+    
+      this.setState({ joiningRoom: false });
   }
-
-  async addTodo() {
-    if (!this.state.pendingTodo) return;
+  
+  async createRoom() {
+    console.log('creating room', this.state.creatingRoom);
+    if (this.state.creatingRoom) return;
     // Set a flag to indicate loading
-    this.setState({ addingTodo: true });
+    this.setState({ creatingRoom: true });
     // Add a new todo from the value of the input
-    await firestore.collection("todos").add({
-      content: this.state.pendingTodo,
-      completed: false,
-      createdAt: new Date().toISOString()
+    var that = this;
+    console.log('make req');
+    await firestore.collection("rooms").doc(this.getHash()).set({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      private: false,
+      playersInLobby: 1
     });
+    console.log('req finished');
     // Remove the loading flag and clear the input
-    this.setState({ addingTodo: false, pendingTodo: "" });
+    this.setState({ creatingRoom: false });
   }
 
   render() {
     return (
       <Layout className="App">
         <Header className="App-header">
-          <h1>Quick Todo</h1>
+          <h1>NERF Game</h1>
         </Header>
         <Content className="App-content">
-          <Input
-            ref="add-todo-input"
-            className="App-add-todo-input"
-            size="large"
-            placeholder="What needs to be done?"
-            disabled={this.state.addingTodo}
-            onChange={evt => this.setState({ pendingTodo: evt.target.value })}
-            value={this.state.pendingTodo}
-            onPressEnter={this.addTodo}
-            required
-          />
           <Button
-            className="App-add-todo-button"
+            className="App-create-room-button"
             size="large"
             type="primary"
-            onClick={this.addTodo}
-            loading={this.state.addingTodo}
+            onClick={this.createRoom}
+            loading={this.state.creatingRoom}
           >
-            Add Todo
+            Create Room
           </Button>
           <List
-            className="App-todos"
+            className="App-rooms"
             size="large"
             bordered
-            dataSource={this.state.todos}
-            renderItem={todo => (
+            dataSource={this.state.rooms}
+            renderItem={room => (
               <List.Item>
-                {todo.content}
-                <Icon
-                  onClick={evt => this.completeTodo(todo.id)}
-                  className="App-todo-complete"
-                  type="check"
-                />
+                {room.friendlyCode}
+                &nbsp;
+                <Button
+                  onClick={evt => this.joinRoom(room.id)}
+                  className="App-join-room-button"
+                  size="large"
+                  type="primary"
+                  loading={this.state.joiningRoom}
+                >
+                  Join Room with {room.playersInLobby} other(s)
+                </Button>
               </List.Item>
             )}
           />
