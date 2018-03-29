79beb1b0ca2228a3934f1131281d76a105d7eb1f
Eric Beyer <eric.m.beyer@gmail.com>
2018-03-29 08:40:11 -0700

    Add delete functionality

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     // Set the default state of our application
-    this.state = { creatingRoom: false, joiningRoom: false, rooms: [] };
+    this.state = { creatingRoom: false, joiningRoom: false, deletingRoom: false, rooms: [] };
     // We want event handlers to share this context
     this.createRoom = this.createRoom.bind(this);
+    this.deleteRoom = this.deleteRoom.bind(this);
     this.joinRoom = this.joinRoom.bind(this);
     // We listen for live changes to our todos collection in Firebase
     firestore.collection("rooms").onSnapshot(snapshot => {
@@ -25,10 +26,10 @@ class App extends Component {
         room.id = doc.id;
         if (!room.private) rooms.push(room);
       });
-      // Sort our todos based on time added
+      // Sort our rooms based on time added
       rooms.sort(function(a, b) {
         return (
-          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+          new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
         );
       });
       // Anytime the state of our database changes, we update state
@@ -87,6 +88,17 @@ class App extends Component {
     // Remove the loading flag and clear the input
     this.setState({ creatingRoom: false });
   }
+  
+  async deleteRoom(id) {
+    console.log('deleting room');
+    if (this.state.deletingRoom) return;
+    
+    this.setState({deletingRoom: true});
+    
+    await firestore.collection("rooms").doc(id).delete();
+    
+    this.setState({ deletingRoom : false});
+  }
 
   render() {
     return (
@@ -111,7 +123,7 @@ class App extends Component {
             dataSource={this.state.rooms}
             renderItem={room => (
               <List.Item>
-                {room.friendlyCode}
+                {room.id}
                 &nbsp;
                 <Button
                   onClick={evt => this.joinRoom(room.id)}
@@ -121,6 +133,15 @@ class App extends Component {
                   loading={this.state.joiningRoom}
                 >
                   Join Room with {room.playersInLobby} other(s)
+                </Button>
+                <Button
+                  onClick={evt => this.deleteRoom(room.id)}
+                  className="App-delete-room-button"
+                  size="large"
+                  type="danger"
+                  loading={this.state.deletingRoom}
+                >
+                  Delete
                 </Button>
               </List.Item>
             )}
